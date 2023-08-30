fc5fcd9b14798de75ee99b94df71977a4db32c1b
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-08-30 11:13:30 +0530

    updates

--- Test-1-practice/Q-3.js
+++ Test-1-practice/Q-3.js
@@ -2,16 +2,73 @@
 // Write a JavaScript function that takes an array of numbers
 //  as input and returns the smallest number from the array.
 
-
 var numbers = [7, 4, 5, 7, 8, 9, 5, 3, 6, 8, 4, 7, 9, 3, 6, 8, 2];
+
 function findSmallNumber(array) {
-    var minimum = array[0]; //  7 - 4 -  3 - 2
-    for (var i = 1; i < array.length; i++) {
-        // console.log(minimum, array[i], minimum > array[i])
-        if (minimum > array[i]) {
-            minimum = array[i]; // 7 -4 - 3 - 2
-        }
+  var minimum = array[0]; //  7 - 4 -  3 - 2
+  for (var i = 1; i < array.length; i++) {
+    // console.log(minimum, array[i], minimum > array[i])
+    if (minimum > array[i]) {
+      minimum = array[i]; // 7 -4 - 3 - 2
     }
-    return minimum
+  }
+  return minimum;
 }
-console.log(findSmallNumber(numbers))
+
+console.log(findSmallNumber(numbers));
+
+
+
+
+var numbers = [
+  456, 2125, 32578, 31545, 121, 21553, 3854, 55544, 321545, 488, 8865, 5484,
+  1545,
+];
+
+function findSmallNumber(array) {
+  var minimum = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (minimum > array[i]) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
+}
+
+console.log(findSmallNumber(numbers));
+
+
+
+
+var numbers = [
+  1254, 2365, 23698, 7895, 478996, 41254, 12, 54698, 5469, 42656, 98,
+];
+
+function findSmallNumber(array) {
+  var minimum = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (minimum > array[i]) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
+}
+
+console.log(findSmallNumber(numbers));
+
+
+
+
+var numbers = [25, 121, 45, 36, 25, 89, 78, 456, 365, 21, 36, 325, 89];
+
+function findSmallNumber(array) {
+  var minimum = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (minimum > array[i]){
+        minimum = array[i];
+    }
+  }
+  return minimum;
+}
+
+console.log(findSmallNumber(numbers));
--- Test-1-practice/Q-4.js
+++ Test-1-practice/Q-4.js
@@ -3,19 +3,79 @@
 //  as input and returns a new array containing elements
 //   that are present in both arrays.
 
-var array1 = [1, 2, 34, 5, 6]
-var array2 = [2, 5, 6, 7, 8]
+var array1 = [1, 2, 34, 5, 6];
+var array2 = [2, 5, 6, 7, 8];
 
 function findSame(array1, array2) {
-    var newArray = [];
-    for (var i = 0; i < array1.length; i++) {
-        for (var j = 0; j < array2.length; j++) {
-            if (array1[i] == array2[j]) {
-                newArray.push(array1[i])
-            }
-        }
+  var newArray = [];
+  for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[j]) {
+        newArray.push(array1[i]);
+      }
     }
-    return newArray;
+  }
+  return newArray;
 }
 
-console.log(findSame(array1, array2))
+console.log(findSame(array1, array2));
+
+
+
+
+var array1 = [5, 6, 89, 35, 45, 36];
+var array2 = [84, 96, 5, 48, 2, 69];
+
+function findSame(array1, array2) {
+  var newArray = [];
+  for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2; j++) {
+      if (array1[i] == array2[j]) {
+        newArray.push(array1[i]);
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(findSame(array1, array2));
+
+
+
+
+var array1 = [8,5,9,69,5,85];
+var array2 = [,58,9966,448,4465];
+
+function findSame(array1, array2) {
+  var newArray = [];
+  for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[j]) {
+        newArray.push(array1[i]);
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(findSame(array1, array2));
+
+
+
+
+var array1 = [566848,5151,3236,365,89,78];
+var array2 = [8,9,55,9,1484,226];
+
+function findSame(array1, array2) {
+  var newArray = [];
+  for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[j]) {
+        newArray.push(array1[i]);
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(findSame(array1, array2));
