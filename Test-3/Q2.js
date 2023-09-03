b960a338673107c3b01361dcf167903f7f193159
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-09-03 17:36:31 +0530

    updates

---
+++ Test-3/Q2.js
@@ -0,0 +1,13 @@
+var input = 4;
+
+function printStars(number) {
+    for (var i = 1; i <= number; i++) {
+        var pattern = ""
+        for (var j = 1; j <= i; j++) {
+            pattern = pattern + "$"
+        }
+        console.log(pattern)
+    }
+}
+
+printStars(input)
